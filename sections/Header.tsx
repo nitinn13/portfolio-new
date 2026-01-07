5629eb958556d2ed4e6cf085d8a19a06aa5e1085
nitinn13 <nitinjha080@gmail.com>
2026-01-07 23:17:00 +0530

    update pfp

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,16 +1,20 @@
+import Image from "next/image";
 export default function Header() {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center gap-10 w-full">
       <div className="flex flex-col items-center gap-6 w-full max-w-[640px] p-8 border border-dashed border-gray-200 border-t-0">
         <div className="flex flex-row items-center justify-center gap-2.5">
-          <div className="w-[70px] h-[70px] rounded-[10px] overflow-hidden">
-            <img
-              src="https://framerusercontent.com/images/5unPgKWtdscBGaxmLqBsg71bADA.jpg?width=400&height=400"
+          <div className="w-[70px] h-[70px] rounded-[10px] overflow-hidden relative">
+            <Image
+              src="/pfp.png"
               alt="Profile"
-              className="w-full h-full object-cover object-top"
+              fill
+              className="object-cover object-top"
+              priority
             />
           </div>
         </div>
+
         <div className="flex flex-col items-center gap-1.5 w-full">
           <h1 className="text-[28px] font-semibold tracking-[-0.05em] text-center text-[#121212]">
             Nitin Jha
